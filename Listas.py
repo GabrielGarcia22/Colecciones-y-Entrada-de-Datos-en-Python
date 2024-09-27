edb9c918069fcc3bd960e97831fbacd53dcb83b0
GabrielGarcia22 <gg21-0539@unphu.edu.do>
2024-09-27 16:16:13 -0400

    Agregue funciones

--- Listas.py
+++ Listas.py
@@ -1,3 +1,20 @@
 # Creacion de nuestra lista
+miLista = [15, 33, 47, 88, 66]
 
-g
+# Acceso por indice en nuestra lista
+
+def acceso_indice():
+    print(" ")
+    print("Elemento del indice 1: ", miLista[1])  # La salida sera el numero 33
+
+
+# Modificacion de elementos
+def modificacion_elementos():
+    print(" ")
+    print("Elemento sin modificar: ", miLista[2])
+    miLista[2] = 11
+    print("Elemento del indice 2 modificado: ", miLista[2])
+    
+
+modificacion_elementos()
+
