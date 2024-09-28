6561eeb2d16f2b93fefda84fd82924b279a78a70
GabrielGarcia22 <gg21-0539@unphu.edu.do>
2024-09-28 10:42:54 -0400

    Anadi EntraDatos.py

---
+++ EntradaDatos.py
@@ -0,0 +1,21 @@
+def Entrada_Nombre():
+    print(" ")
+    nombre = input("Cual es tu nombre? ")
+    print(f"Tu nombre es {nombre}")
+    print(" ")
+    
+def entrada_convertida():
+    print(" ")
+    edad = int(input("Cual es tu edad? "))
+    print(f"Tu edad es {edad} ")
+    print(" ")
+    
+    estatura = float(input("Cual es tu estatura "))
+    print(f"Tu estatura es {estatura}")
+    print(" ")
+    
+    
+    
+entrada_convertida()
+    
+    
