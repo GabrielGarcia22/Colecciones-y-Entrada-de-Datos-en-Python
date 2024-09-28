ec8b48528c4c52a0eb2e8dff2924d79c96c3371d
GabrielGarcia22 <gg21-0539@unphu.edu.do>
2024-09-28 10:49:40 -0400

    Anadi AsignacionPractica.py

---
+++ AsignacionPractica.py
@@ -0,0 +1,9 @@
+def Asignacion_1():
+    Usuario = ("Gabriel", "Garcia", 23,)
+    print(" ")
+    print(f"Nombre: {Usuario[0]}")
+    print(f"Apellido: {Usuario[1]}")
+    print(f"Edad: {Usuario[2]}")
+    print(" ")
+    
+Asignacion_1()
