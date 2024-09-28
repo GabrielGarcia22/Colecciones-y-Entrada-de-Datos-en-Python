8b5e767cdbac3b9f3bb01c80599a77ef366c9b4f
GabrielGarcia22 <gg21-0539@unphu.edu.do>
2024-09-28 12:47:54 -0400

    Jastian Anadio Asignacion3.py

--- EntradaDatos.py
+++ EntradaDatos.py
@@ -1,17 +1,17 @@
 def Entrada_Nombre():
     print(" ")
     nombre = input("Cual es tu nombre? ")
-    print(f"Tu nombre es {nombre}")
+    print(f"Tu nombre es: {nombre}")
     print(" ")
     
 def entrada_convertida():
     print(" ")
     edad = int(input("Cual es tu edad? "))
-    print(f"Tu edad es {edad} ")
+    print(f"Tu edad es: {edad} ")
     print(" ")
     
-    estatura = float(input("Cual es tu estatura "))
-    print(f"Tu estatura es {estatura}")
+    estatura = float(input("Cual es tu estatura? "))
+    print(f"Tu estatura es: {estatura}")
     print(" ")
     
     
