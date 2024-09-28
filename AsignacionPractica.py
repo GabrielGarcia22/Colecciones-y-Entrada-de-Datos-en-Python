f8494766e2329b60f821075ec02e3bc48e0e9686
GabrielGarcia22 <gg21-0539@unphu.edu.do>
2024-09-28 11:13:06 -0400

    Pequenos cambios

--- AsignacionPractica.py
+++ AsignacionPractica.py
@@ -8,6 +8,7 @@ vehiculos = {
 def asignacion_1():
     usuario = ("Gabriel", "Garcia", 23,)
     print(" ")
+    usuario[0] = "Miguel"
     print(f"Nombre: {usuario[0]}")
     print(f"Apellido: {usuario[1]}")
     print(f"Edad: {usuario[2]}")
@@ -15,12 +16,22 @@ def asignacion_1():
     
 
 def asignacion_2(matricula):
+    
+    vehiculos = {
+        "ABC125" : {"modelo": "Toyota Corolla", "año": 2015},
+        "CBA725" : {"modelo": "Honda Fit", "año": 2017},
+        "BAC125" : {"modelo": "Honda Accord", "año": 2012},
+        "CAB128" : {"modelo": "Hyundai Sonata", "año": 2020},
+    }
+    
     return vehiculos.get(matricula, "Vehiculo no encontrado")
 print(" ")   
 matricula = input("Cual es la matricula de su vehiculo? ")
 info_vehiculo = asignacion_2(matricula)
+print(" ")
 print(f"Informacion del vehiculo con matricula {matricula}: {info_vehiculo}")
 print(" ")
-    
+
+
     
 asignacion_2(matricula)
